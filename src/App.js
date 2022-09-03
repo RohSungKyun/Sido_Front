9b2b0acdd3c6886fcfcd637e04575584eb21ac33
yaena1223 <yaena1223@dgu.ac.kr>
2022-09-03 13:01:54 +0900

    0903 React 초기셋팅

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import App_Router from "./routes/Router";
+
+function App() {
+    return (
+        <div className="App">
+            <App_Router />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/routes/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Signup(props) {
+    return (
+        <>
+            <h1>회원가입페이지</h1>
+        </>
+    );
+}
+
+export default Signup;
